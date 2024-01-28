120dbea90faa98995fe2bce2272d200925c88b55
akmmp241 <akm.mp24@gmail.com>
2024-01-28 20:46:51 +0700

    feat: create login page

---
+++ app/login/page.jsx
@@ -0,0 +1,68 @@
+import {Input} from "@/components/ui/input"
+import {Label} from "@/components/ui/label"
+import {Button} from "@/components/ui/button"
+import Link from "next/link"
+import Image from "next/image";
+import GoogleIcon from "./assets/Google-logo.png"
+
+export default function Register() {
+  const inputClass = "w-full sm:w-[320px] lg:w-[400px] py-2 lg:py-3 pl-4 bg-transparent outline-1 outline outline-primary focus-visible:outline-blue rounded-lg text-sm"
+
+  return (
+      <>
+        <div className={"text-primary text-3xl lg:text-4xl font-bold"}>
+          <h1>Masuk ke Akun Kamu</h1>
+        </div>
+        <div>
+          <form className={"flex flex-col gap-6 text-primary"}>
+            <div className={"flex flex-col gap-1.5"}>
+              <Label className={"text-sm"} htmlFor="Email">Email</Label>
+              <Input
+                  className={inputClass}
+                  type={"text"}
+                  placeholder={"Email Anda di sini"}
+              />
+            </div>
+            <div className={"flex flex-col gap-1.5"}>
+              <label className={"text-sm"} htmlFor="password">Kata sandi</label>
+              <Input
+                  className={inputClass}
+                  type={"text"}
+                  placeholder={"Email Anda di sini"}
+              />
+            </div>
+            <div>
+              <Button
+                  type={"submit"}
+                  size={"lg"}
+                  className={"w-full bg-blue rounded-lg mt-2 font-bold"}>
+                Masuk
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div className={"flex flex-col gap-6"}>
+          <div className={"flex flex-row justify-between items-center"}>
+            <span className={"w-1/2 h-[1px] bg-primary border-t border-primary"}></span>
+            <span className={"w-full mx-4 text-center text-sm"}>Atau lanjutkan dengan</span>
+            <span className={"w-1/2 h-[1px] bg-primary border-t border-primary"}></span>
+          </div>
+          <div>
+            <Button className={"gap-2.5 w-full sm:w-[320px] lg:w-[400px] py-3 lg:py-4 pl-4 bg-transparent outline-1 outline outline-primary active:bg-primary active:text-black"}>
+              <Image
+                  src={GoogleIcon}
+                  alt={"sign in with Google"}
+                  width={20}
+                  height={20}
+              />
+              Masuk dengan Google
+            </Button>
+          </div>
+        </div>
+        <div className={"self-center"}>
+          <span className={"mr-1.5"}>Belum memiliki akun?</span>
+          <Link href={"/register"} className={"text-blue"}>Daftar Sekarang</Link>
+        </div>
+      </>
+  )
+}
---
+++ app/login/template.jsx
@@ -0,0 +1,4 @@
+import "../register/style.css"
+import Template from "@/app/register/template";
+
+export default Template
